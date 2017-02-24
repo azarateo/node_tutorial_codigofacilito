e4c0b0de1294fd0de6cb9d993b83959f481824d8
azarateo <azarateo@gmail.com>
2017-02-23 20:52:19 -0500

    Retreive JSON Checked with file read

--- hello.js
+++ hello.js
@@ -1,11 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 
-var servidor = http.createServer(
-	function(request, response){
-		response.writeHead(200,{"Context-Type":"application/json"});
-		response.write(JSON.stringify({nombre:"Alejandro",apellido:"Zarate"}));
-		response.end();
+fs.readFile('./index.html',);
+
+var dataRead = function(err,file){
+	if(err){console.log(err);}
+	else{
+		var servidor = http.createServer(
+			function(request, response){
+				response.writeHead(200,{"Context-Type":"application/json"});
+				response.write(JSON.stringify({nombre:"Alejandro",apellido:"Zarate"}));
+				response.end();
+			}
+		);
+		servidor.listen(9000);
 	}
-);
-servidor.listen(9000);
+}
+
