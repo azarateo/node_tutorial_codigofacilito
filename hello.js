31ccfb6a186dc010fd779148776727a24e426b0e
azarateo <azarateo@gmail.com>
2017-02-23 20:36:56 -0500

    Retreive JSON Checked

--- hello.js
+++ hello.js
@@ -2,17 +2,10 @@ var http = require('http');
 var fs = require('fs');
 
 var servidor = http.createServer(
-    function(request, response){
-        response.writeHead(200,{"Context-Type":"application/json"});
-        	response.write(
-            JSON.stringify(
-                            {
-                                   nombre:"Alejandro",
-                                   apellido:"Zarate"
-                            }
-                        );
-         );
-         response(end);
-     }
+	function(request, response){
+		response.writeHead(200,{"Context-Type":"application/json"});
+		response.write(JSON.stringify({nombre:"Alejandro",apellido:"Zarate"}));
+		response.end();
+	}
 );
 servidor.listen(9000);
