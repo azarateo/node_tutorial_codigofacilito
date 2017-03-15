929de02bbe90aaaab18094bb1a338ff65f0aa91b
azarateo <azarateo@gmail.com>
2017-03-15 12:42:02 -0500

    Writing a json file and specifying in the header Content Type

--- hello.js
+++ hello.js
@@ -1,24 +1,14 @@
 var http = require('http');
-var fs = require('fs');
 
+http.createServer(
+    function(req,res){
+    		var jsonresult = JSON.stringify({nombre: "Alejandro", apellido: "Zarate"});
+            res.writeHead(200,{"Content-Type":"application/json"});
+            res.write(jsonresult);
+            res.end();
+    }
+).listen(9000);
 
-
-var servidor = http.createServer(function(request, response){
-	fs.readFile("./index.html",(err,file)=>{
-			if(err){console.log(err);}
-			else{
-				response.writeHead(200,{"Context-Type":"application/json"});
-				var i = 0;
-				while(true){
-					response.write(i+"");
-					i++;
-				}
-
-				response.end();
-			}
-	});
-});
-servidor.listen(9000);
 
 
 
