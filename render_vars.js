dc2671481b5448987b78651aa9d850d8b51299d2
azarateo <azarateo@gmail.com>
2017-03-22 19:59:32 -0500

    Video 5 - Preprocessing html and replacing variables

--- render_vars.js
+++ render_vars.js
@@ -4,8 +4,19 @@ var fs = require('fs');
 http.createServer(
     function(req,res){
     		fs.readFile('./index.html',function(err,html){
+    			var html_string = html.toString();
+    			var variables = html_string.match(/[^\{\}]+(?=\})/g);
+    			//The regular expression means a text that is not one or more { or }
+    			//and that searching ahead finds }
+
+    			for (var i = variables.length - 1; i >= 0; i--) {
+    				var miVariable = eval(variables[i]);
+    				html_string.replace("{"+variables[i]+"}",miVariable);
+    			}
+
+
             	res.writeHead(200,{"Content-Type":"text/html"});
-            	res.write(html);
+            	res.write(html_string);
             	res.end();
     		});
     		
