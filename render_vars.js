8484e908806118eecde208a991aaa9e460555b40
azarateo <azarateo@gmail.com>
2017-03-22 20:01:22 -0500

    Video 5 - Preprocessing html and replacing variables

--- render_vars.js
+++ render_vars.js
@@ -6,6 +6,7 @@ http.createServer(
     		fs.readFile('./index.html',function(err,html){
     			var html_string = html.toString();
     			var variables = html_string.match(/[^\{\}]+(?=\})/g);
+    			var nombre = 'Alejo';
     			//The regular expression means a text that is not one or more { or }
     			//and that searching ahead finds }
 
