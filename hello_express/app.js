6136e75a6c7e35bfa05343ec90d4493404733c98
azarateo <azarateo@gmail.com>
2017-03-25 18:41:32 -0500

    Video 10 fin

--- hello_express/app.js
+++ hello_express/app.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.set("view engine","jade");	
 app.get("/",function(req,res) {
-	res.render("index");
+	res.render("index",{variable:"Hola alejandrito"});
 });
 
 
 app.listen(8080);
 
-//Video 10 using Jade
+//Video 10 using Jade fin
