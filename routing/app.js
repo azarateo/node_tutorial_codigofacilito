633904b76081c0c3dd32f56f082b6f18c972a770
azarateo <azarateo@gmail.com>
2017-04-03 21:22:43 -0500

    Video 12. Button to home, with Express and jade. Using parameters in url and printing in the view

--- routing/app.js
+++ routing/app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 
 app.set("view engine","jade");
-
-app.get("/",function(req,res) {
-	res.render("index");
+//If you load http://localhost:8080/alejo you get a message to alejo
+app.get("/:person",function(req,res) {
+	res.render("index",{variable:"mensajito de prueba a "+req.params.person});
 });
 
 app.post("/",function(req,res) {
